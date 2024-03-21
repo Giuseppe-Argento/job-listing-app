d6808675c99170f207c942e8af52e6818d57fbfd
Giuseppe-Argento <gawebnapoli@gmail.com>
2024-03-21 10:18:58 +0100

    Active Class for nav lonks

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,15 @@ import {
 import MainLayout from './layouts/MainLayout';
 
 import HomePage from './pages/NomePage';
+import JobPage from './pages/JobPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout />}>
       <Route index element={<HomePage />} />
+      <Route path='/jobs' element={<JobPage />} />
+      <Route path='*' element={<NotFoundPage />} />
     </Route>
   )
 );
