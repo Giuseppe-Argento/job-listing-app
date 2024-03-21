0472cf54b1816f37be0301bc9b715b48ee3819d2
Giuseppe-Argento <gawebnapoli@gmail.com>
2024-03-21 08:25:28 +0100

    prova

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import HomeCards from './components/HomeCards'
-import JobListings from './components/JobListings'
-import ViewJobs from './components/ViewJobs'
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider
+} from 'react-router-dom'
+import MainLayout from './layouts/MainLayout';
+
+import HomePage from './pages/NomePage';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+    </Route>
+  )
+);
 
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Hero title='Test Title' subtitle='this is a subtitle' />
-      <HomeCards />
-      <JobListings />
-      <ViewJobs />
+      <RouterProvider router={router} />
     </>
 
   )
--- src/components/JobListing.tsx
+++ src/components/JobListing.tsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import { useState } from 'react'
+import {FaMapMarker} from 'react-icons/fa'
 
 const JobListing = ({job}) => {
-  return (
+
+    let description = job.description
+
+    const [showFullDescription, setShowFullDescription] = useState(false)
+
+
+    if(!showFullDescription){
+        description = description.substring(0, 90) + '...'
+    }
+
+    return (
     <div className="bg-white rounded-xl shadow-md relative">
                             <div className="p-4">
                                 <div className="mb-6">
@@ -10,16 +22,19 @@ const JobListing = ({job}) => {
                                 </div>
 
                                 <div className="mb-5">
-                                    {job.description}
+                                    {description}
                                 </div>
+                                <button onClick={()=> setShowFullDescription((prevState) => (!prevState))} className='mb-5 text-indigo-500 hover:text-indigo-600'>
+                                    {showFullDescription ? 'Less' : 'More'}
+                                </button>
 
                                 <h3 className="text-indigo-500 mb-2">{job.salary}</h3>
 
                                 <div className="border border-gray-100 mb-5"></div>
 
                                 <div className="flex flex-col lg:flex-row justify-between mb-4">
-                                    <div className="text-orange-700 mb-3">
-                                        <i className="fa-solid fa-location-dot text-lg"></i>
+                                    <div className="text-orange-700 mb-3 flex gap-2 items-center">
+                                        <FaMapMarker/>
                                         {job.location}
                                     </div>
                                     <a
