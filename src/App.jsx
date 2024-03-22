fee26c325eede31bedad7b463f338a8857a9589a
Giuseppe-Argento <gawebnapoli@gmail.com>
2024-03-22 09:31:10 +0100

    add job form State

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,20 @@ import {
 } from 'react-router-dom'
 import MainLayout from './layouts/MainLayout';
 
-import HomePage from './pages/NomePage';
-import JobPage from './pages/JobPage';
+import HomePage from './pages/HomePage';
+import JobsPage from './pages/JobsPage';
+import JobPage, {jobLoader} from './pages/JobPage';
 import NotFoundPage from './pages/NotFoundPage';
+import AddJobPage from './pages/AddJobPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout />}>
       <Route index element={<HomePage />} />
-      <Route path='/jobs' element={<JobPage />} />
+      <Route path='/jobs' element={<JobsPage />} />
+      <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader}/>
       <Route path='*' element={<NotFoundPage />} />
+      <Route path='/add-job' element={<AddJobPage />} />
     </Route>
   )
 );
